32a35a6a7bfefc4ae9223e795711b0b5a5d0a5a1
Alquiladora <alquiladora623@gmail.com>
2025-10-02 13:37:27 -0600

    pwa 3

--- sw.js
+++ sw.js
@@ -13,6 +13,7 @@ const urlsToCache = [
   '/icons/icon-256x256.png',
   '/icons/icon-384x384.png',
   '/icons/icon-512x512.png',
+  '/img1.jpg',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
   'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap',
